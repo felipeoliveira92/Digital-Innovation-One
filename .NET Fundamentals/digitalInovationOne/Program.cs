355bc57f4577297537da2f8af8540c510477cb75
felipeoliveira92 <61589016+felipeoliveira92@users.noreply.github.com>
2021-05-19 23:32:47 -0300

    inicando bootcamp .net fundametals

---
+++ .NET Fundamentals/digitalInovationOne/Program.cs
@@ -0,0 +1,17 @@
+﻿using System;
+
+namespace digitalInovationOne
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            int numeroVezes = 5;
+
+            for(int i = 0; i <numeroVezes; i++)
+            {
+                Console.WriteLine($"Bem Vindo ao curso de .NET {i}");
+            }
+        }
+    }
+}
