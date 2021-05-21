d41d1ff71d2e0813c954d7f2549e09841518dd25
felipeoliveira92 <61589016+felipeoliveira92@users.noreply.github.com>
2021-05-20 22:43:01 -0300

    using class and parameters

---
+++ .NET Fundamentals/estruturaDoProjeto/Program.cs
@@ -0,0 +1,12 @@
+﻿using System;
+
+namespace estruturaDoProjeto
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Hello World!");
+        }
+    }
+}
