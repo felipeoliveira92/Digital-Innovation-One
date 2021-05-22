e6918ca6331893137c8d8d695e34bc385fc93192
felipeoliveira92 <61589016+felipeoliveira92@users.noreply.github.com>
2021-05-22 15:32:29 -0300

    development project 1

---
+++ .NET Fundamentals/DioBank/Program.cs
@@ -0,0 +1,13 @@
+﻿using System;
+
+namespace DioBank
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Conta minhaConta = new Conta(TipoConta.PessoaFisica, 0, 0, "Felipe Rabelo");
+            Console.WriteLine("Hello World!");
+        }
+    }
+}
