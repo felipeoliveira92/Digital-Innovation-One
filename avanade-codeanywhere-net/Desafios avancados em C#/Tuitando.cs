3e51e49acffd7dfec9a5f8b947a926a2ca1bfaf3
felipeoliveira92 <61589016+felipeoliveira92@users.noreply.github.com>
2021-08-28 12:28:47 -0300

    create Tuitando

---
+++ avanade-codeanywhere-net/Desafios avancados em C#/Tuitando.cs
@@ -0,0 +1,17 @@
+﻿using System;
+
+class Tuitando
+{
+    static void Main(string[] args)
+    {
+
+        string v = Console.ReadLine();
+        char[] arr = v.ToCharArray();
+        if (arr.Length <= 140) //complete a condicional
+            Console.WriteLine("TWEET");
+        else
+            Console.WriteLine("MUTE");
+
+    }
+
+}
