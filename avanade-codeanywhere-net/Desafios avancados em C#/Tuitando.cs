843277f0b23aa3b8647249895c0f1ebdb4603367
felipeoliveira92 <61589016+felipeoliveira92@users.noreply.github.com>
2021-08-28 12:29:36 -0300

    clean code

--- avanade-codeanywhere-net/Desafios avancados em C#/Tuitando.cs
+++ avanade-codeanywhere-net/Desafios avancados em C#/Tuitando.cs
@@ -4,14 +4,15 @@ class Tuitando
 {
     static void Main(string[] args)
     {
-
         string v = Console.ReadLine();
         char[] arr = v.ToCharArray();
-        if (arr.Length <= 140) //complete a condicional
+        if (arr.Length <= 140)
+        {
             Console.WriteLine("TWEET");
+        }
         else
+        {
             Console.WriteLine("MUTE");
-
+        }
     }
-
 }
