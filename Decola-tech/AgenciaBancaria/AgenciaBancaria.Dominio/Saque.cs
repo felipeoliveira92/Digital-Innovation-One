68f79357854aeeb738d082dda494a0b4d8a376f9
felipeoliveira92 <61589016+felipeoliveira92@users.noreply.github.com>
2021-08-31 19:15:25 -0300

    Criando um dominio usando os pilares da orientacao objetos

---
+++ Decola-tech/AgenciaBancaria/AgenciaBancaria.Dominio/Saque.cs
@@ -0,0 +1,17 @@
+﻿
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace AgenciaBancaria.Dominio
+{
+    public class Saque : Lancamento
+    {
+        public Saque(decimal valor, DateTime data, ContaBancaria conta) : base(valor, data, conta)
+        {
+
+        }
+    }
+}
