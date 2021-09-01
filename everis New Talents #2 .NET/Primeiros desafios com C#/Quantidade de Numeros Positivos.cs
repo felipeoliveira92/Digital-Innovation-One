87cc7cd09b4810a50b183c7b74647c242f9a17f2
felipeoliveira92 <61589016+felipeoliveira92@users.noreply.github.com>
2021-09-01 13:37:52 -0300

    Create Quantidade de Numeros Positivos.cs

---
+++ everis New Talents #2 .NET/Primeiros desafios com C#/Quantidade de Numeros Positivos.cs
@@ -0,0 +1,38 @@
+            double valor1 = double.Parse(Console.ReadLine());
+            double valor2 = double.Parse(Console.ReadLine());
+            double valor3 = double.Parse(Console.ReadLine());
+            double valor4 = double.using System;
+
+    class QuantidadePositivo
+    {
+        static void Main(string[] args)
+        {
+          double[] valores = new double[6];
+          
+          for(int i=0; i<6; i++)
+          {
+            valores[i] = double.Parse(Console.ReadLine());
+          }
+          
+          int ehPositivo = 0;
+          
+          for(int i=0; i<6; i++)
+          {
+            if (valores[i] >0.0)
+            {
+                ehPositivo += 1;
+            }
+          }
+
+          Console.WriteLine($"{ehPositivo} valores positivos");
+        }
+    }Parse(Console.ReadLine());
+            double valor5 = double.Parse(Console.ReadLine());
+            double valor6 = double.Parse(Console.ReadLine());
+          
+            int ehPositivo = 0;
+            
+            if (valor1 >0.0)
+            {
+                ehPositivo += 1;
+            }
