200eea4904837bbd3b5b488159734600395d568e
felipeoliveira92 <61589016+felipeoliveira92@users.noreply.github.com>
2021-09-01 11:53:55 -0300

    resolucao desafios everis new talents

---
+++ everis New Talents #2 .NET/Desafios Introdutorios com C#/Tuitando.cs
@@ -0,0 +1,18 @@
+using System;
+
+class Tuitando
+{
+    static void Main(string[] args)
+    {
+        string v = Console.ReadLine();
+        char[] arr = v.ToCharArray();
+        if (arr.Length <= 140)
+        {
+          Console.WriteLine("TWEET");
+        }
+        else
+        {
+          Console.WriteLine("MUTE");
+        }
+    }
+}
---
+++ everis New Talents #2 .NET/Primeiros Passos com C#/Soma Simples.cs
@@ -0,0 +1,14 @@
+using System;
+
+class SomaSimples
+{
+    static void Main(string[] args)
+    {
+        int A = Convert.ToInt32(Console.ReadLine());
+        int B = Convert.ToInt32(Console.ReadLine());
+        
+        int SOMA = A + B;
+
+        Console.WriteLine($"SOMA = {SOMA}");
+    }
+}
